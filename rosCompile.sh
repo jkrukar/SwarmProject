8c32fea20aa33f16ddfab27bf798505a81b3152f
Brendan Donohoe <bddonohoe@unm.edu>
2017-11-29 13:07:26 -0700

    Uploaded a new version of the repository.
    Uploaded my version of the repository, as requested, due to strange issues folks seem to be having with the previous version where the robots would spin around in circles.  Look at the README beforehand to do any configuring that may need to be taken care of so that it works on your machine.  I would also suggest adding the non-source folders to the gitignore so we aren't committing i.e. logs and other files that don't really have to do with the source code.

--- rosCompile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if ! grep -q "source /opt/ros/kinetic/setup.bash" ~/.bashrc
-then 
-  echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-fi
-source ~/.bashrc
--- src/behaviours/src/ObstacleController.cpp
+++ src/behaviours/src/ObstacleController.cpp
@@ -121,7 +121,7 @@ void ObstacleController::ProcessData() {
     }
   }
   else {
-    if (center < 0.12) {
+    if (center < 3.0) {
       result.wristAngle = 0.7;
     }
     else {
@@ -177,16 +177,14 @@ bool ObstacleController::checkForCollectionZoneTags( vector<Tag> tags ) {
     // Check the orientation of the tag. If we are outside the collection zone the yaw will be positive so treat the collection zone as an obstacle. If the yaw is negative the robot is inside the collection zone and the boundary should not be treated as an obstacle. This allows the robot to leave the collection zone after dropping off a target.
     if ( tag.calcYaw() > 0 ) 
       {
-        std::cout << "yaw: " << tag.calcYaw() << ";" << std::endl;
+      std::cout << "yaw " << tag.calcYaw() << ";" << std::endl;
 	// checks if tag is on the right or left side of the image
 	if (tag.getPositionX() + camera_offset_correction > 0) {
 	  count_right_collection_zone_tags++;
 	  
-    //t_collection_zone_tags++;
+	} else {
+	  count_left_collection_zone_tags++;
 	}
-        else {
-          count_left_collection_zone_tags++;
-        }
       }
     
   }
